6596d2c3205658427d192caf93842ce835b25093
Kyoya27 <mouadh95@live.fr>
2018-01-10 12:28:43 +0100

    Models Data 10/01/18

---
+++ galaxy_explorer/app/controllers/destinations_controller.rb
@@ -0,0 +1,46 @@
+class DestinationsController < ApplicationController
+	before_action :set_destination, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@destinations = Destination.all
+	end
+	def show; end
+	def new
+		@destination = Destination.new
+	end
+	def create
+		@destination = Destination.new(require_destination)
+		if @destination.save
+			redirect_to(@destination)
+		else
+			render 'new'
+		end
+	end
+	def edit; end
+	def update
+		if @destination.update(require_destination)
+		redirect_to(@destination)
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@destination.destroy
+		redirect_to(destinations_path)
+	end
+
+	private
+	def set_destination
+		@destination = Destination.find(params[:id])
+	end
+
+	def require_destination
+			params.require(:destination).permit(
+				:name,
+				:x,
+				:y,
+        :coeff_touristic
+			)
+	end
+
+end
---
+++ galaxy_explorer/app/models/transports.rb
@@ -0,0 +1,3 @@
+class Transports < ApplicationRecord
+
+end
