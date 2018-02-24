1658ded34d6ba52d84f4b1d5bc2bda455b9f071a
Huskky <morabi.andrea@gmail.com>
2018-02-24 14:36:34 +0100

    Ajout placetobe

--- galaxy_explorer/config/routes.rb
+++ galaxy_explorer/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root to: 'destinations#home'
 	resources :destinations #, only: [:index, :show]
-
+	resources :placetobes
 	resources :transportss #, only: [:index, :show]
   resources :travels #, only: [:index, :show]
 
